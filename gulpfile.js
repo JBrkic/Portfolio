4046d0142e369bf289874571b4a54cf6e865828c
JBrkic <josip.brkic08@gmail.com>
2017-10-31 19:01:34 +0100

    Configured Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
-const gulp        = require('gulp');
+const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
-const sass        = require('gulp-sass');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
+const gutil = require('gulp-util');
 
 // Compile Sass & Inject Into Browser
-gulp.task('sass', function() {
+gulp.task('sass', function () {
     return gulp.src(['src/scss/*.scss'])
+        .pipe(plumber({
+            errorHandler: function (err) {
+                notify.onError({
+                    title: "Gulp error in " + err.plugin,
+                    message: err.toString()
+                })(err);
+
+                // play a sound once
+                gutil.beep();
+            }
+        }))
         .pipe(sass())
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
@@ -12,9 +26,9 @@ gulp.task('sass', function() {
 
 
 // Watch Sass & Serve
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['sass'], function () {
     browserSync.init({
-        server: "./src"  
+        server: "./src"
     });
 
     gulp.watch(['src/scss/*.scss'], ['sass']);
